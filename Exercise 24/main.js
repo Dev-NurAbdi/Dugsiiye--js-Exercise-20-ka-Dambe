b6c967dc860aed3fa0a9b11ebb04a78703422b21
nurabdi2776 <nurabdi2776@gmail.com>
2026-04-04 08:06:25 -0700

    Exercise 24

---
+++ Exercise 24/main.js
@@ -0,0 +1,7 @@
+function calculateArea (width, height = width){
+    return  width * height;
+}
+// asagoo height so basin by default uqatay width square lagu sameyay.
+console.log(calculateArea(5)); // output = 25   
+// asago width iyo height so qatay kadib square lasameyay.
+// console.log(calculateArea(6,6)); // output = 36   
