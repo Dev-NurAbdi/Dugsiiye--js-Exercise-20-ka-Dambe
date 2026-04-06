a7071aadf33507cadc67d7f08afb89d3f23bf431
nurabdi2776 <nurabdi2776@gmail.com>
2026-04-06 07:45:00 -0700

    Exercise 25

--- Exrecise 25/main.js
+++ Exrecise 25/main.js
@@ -1,9 +1,13 @@
+
+
 // spread operator
-const array1 = [1,2,3];
-const allArrays = [...array1,4,5,6];
-console.log(allArrays)
+let array1 = [1,2,3,];
+let array2 = [4,5,6];
+const Allarrays = [...array1,array2];
+console.log(Allarrays);
 // Rest operator
 function multiply(...numbers){
     return numbers.reduce((total,num)=> total * num, 1)
 }
-console.log(multiply(10,10));
+console.log(multiply(6,6))
+
