bc3f2deeb8861309aa81a0f2ff4d995e5ea30d7e
nurabdi2776 <nurabdi2776@gmail.com>
2026-04-23 02:09:36 -0700

    Exercise 32

---
+++ Exercise 32/main.js
@@ -0,0 +1,14 @@
+const title = document.querySelector('#title');
+console.log(title)
+
+const text = document.querySelectorAll('.text');
+console.log(text)
+
+const box = document.querySelectorAll('.box');
+console.log(box)
+
+const btn = document.querySelector('#btn');
+console.log(btn)
+
+const list = document.querySelectorAll('#list');
+console.log(list)
