049f5c78783fa25023ba6d99e49864e921035daa
nurabdi2776 <nurabdi2776@gmail.com>
2026-05-03 07:35:04 -0700

    Exercise 33

---
+++ Exercise 33/main.js
@@ -0,0 +1,9 @@
+const qoraal = document.querySelector(".qoraal")
+
+function ChangeContent(){
+    qoraal.textContent = "Samir ayaa ah furaha guusha."
+}
+
+function ChangeElement(){
+    Oraah.innerHTML = "Hadalku mararka qaar waa xal."
+}
